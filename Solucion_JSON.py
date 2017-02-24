11b0ae6e5c9126b0b5c6e3cdb18f6ef384f1014b
Naxilde <aster9812@gmail.com>
2017-02-24 12:54:28 +0100

    Update Solucion_JSON.py

--- Solucion_JSON.py
+++ Solucion_JSON.py
@@ -1,1 +1,15 @@
 # -*- coding: utf-8 -*-
+import json
+with open("cards.json") as archivo:
+  datos = json.load(archivo)
+
+#filtro = []
+#for e in (datos):
+#  if e["type"] not in filtro:
+#    print e["type"]
+#    filtro.append(e["type"])
+for e in (datos):
+  if e["type"]!="HERO_POWER":
+    if e["set"]!="CHEAT":
+      if e["playerClass"]=="PRIEST":
+        print e["name"]
